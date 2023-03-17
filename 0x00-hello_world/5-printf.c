1e98f144ecba9e1dfe56f6df848deb82f9a83d4a
Alabi <alabiadeyinka719@gmail.com>
2023-03-16 18:30:44 -0700

    My first C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main - Empty point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+            printf("with proper grammar, but the outcome is a price of art, \n");
+        return (0);
+}
