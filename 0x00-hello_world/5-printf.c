89140dd50c0b8411dc51b36728d330a79fa7066e
Alabi <alabiadeyinka719@gmail.com>
2023-03-17 03:54:23 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
 * main - Empty point
 *
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-            printf("with proper grammar, but the outcome is a price of art, \n");
-        return (0);
+	printf("with proper grammar, but the outcome is a price of art, \n");
+	return (0);
 }
